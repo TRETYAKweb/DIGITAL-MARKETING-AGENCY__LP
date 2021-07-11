0bc2d1803e1354093ae2ada783abab76175f1108
tretyakbro <tretyakbro@gmail.com>
2021-07-11 10:37:56 +0300

    Сделал адаптив первого экрана

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(function(){
-  var mixer = mixitup('.portfolio__content');
 
   $('.slider-blog__iner').slick({
     dots:true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
   });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active')
+  });
+
+  var mixer = mixitup('.portfolio__content');
 
 });
